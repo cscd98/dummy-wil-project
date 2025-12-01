543887562ead20ca9a8ff7aafc8f16a70e5334b1
Craig Carnell <1188869+cscd98@users.noreply.github.com>
2025-12-01 22:02:59 +0000

    static test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <wil/resource.h>
 #include <iostream>
 
+int test_static = 0;
+
+WI_HEADER_INITIALIZATION_FUNCTION(TestInit, [] {
+    std::cout << "Lambda invoked at static init\n";
+    test_static = 42;
+    return 1;
+});
+
 int main() {
     std::cout << "Testing WIL with MinGW cross-compile..." << std::endl;
+
+    std::cout << "test static = " << test_static << "\n";
 
     // Force some WIL usage that will break under MinGW
     wil::unique_handle h1;
